846c8e0ae8e7642d62ba62272b5e4812a14814be
Thiago Shigueto Hossaka <126188799+tshigueto@users.noreply.github.com>
2024-02-01 15:49:11 -0300

    Arquivos Projeto Cardápio

--- CardapioVirtual/App.jsx
+++ CardapioVirtual/App.jsx
@@ -1,1 +1,22 @@
-import App
+import {useState} from "react";
+import React from 'react';
+import './App.css';
+import {Navegacao} from './Navegacao';
+import Restaurante from "./assets/restaurante.webp";
+import {ItemCardapio} from "./ItemCardapio";
+import {pratosPrincipais, sobremesas, bebidas} from "./cardapio";
+
+export function App(){
+
+    const paginaMenu = [pratosPrincipais, sobremesas, bebidas];
+    const [paginaSelecionada, atualizarPaginaSelecionada] = useState(0);
+
+    return <>
+          <img src={Restaurante} className="capa"></img>
+          <Navegacao atualizarPaginaSelecionada={atualizarPaginaSelecionada}/>
+          <div className="menu">
+            {paginaMenu[paginaSelecionada].map(item => <ItemCardapio nome={item.nome} descricao={item.descricao} preco={item.preco} imagem={item.imagem}/>)}
+          </div>
+        </>
+    ;
+}
